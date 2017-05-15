dde517010ea4308cf39141fbdbddc24fde5f13bd
wenzhi <wenzhi@mogujie.com>
2017-05-15 16:51:37 +0800

    feature: add muti screens

---
+++ component/concrete/FirstTabOne.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {ListView, View, Text, StyleSheet, TouchableHighlight} from 'react-native'
+export default class FirstTabOne extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello FirstTabOne!</Text>
+      </View>
+    );
+  }
+}
---
+++ component/concrete/FirstTabTwo.js
@@ -0,0 +1,48 @@
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import React, {Component} from 'react';
+
+export default class FirstTabTwo extends Component {
+
+  // static navigationOptions = {
+  //   title: 'Welcome'
+  // };
+
+
+  render() {
+    // const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>
+          Hello FirstTabTwo!
+        </Text>
+      </View>
+    )
+      ;
+  }
+}
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+
